7e30c990cf2fcb81f3e6d0266f68387955d4ff3f
GianmarcoCastilloFlores <gianmarco.castillo@vallegrande.edu.pe>
2025-11-18 12:24:15 -0500

    add project files for challenge

---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
+import { DetalleProductosComponent } from './pages/detalle-productos/detalle-productos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: ListaProductosComponent
+    },
+    {
+        path: 'productos/:id',
+        component: DetalleProductosComponent
+    },
+    {
+        path: 'servicios',
+        component: ServiciosComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
